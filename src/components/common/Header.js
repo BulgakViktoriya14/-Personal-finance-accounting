82825b1ee2a1360b5a662ef6332c52f3a96b4a5f
BulgakViktoriya14 <bulgak.viktoriya@com.com>
2020-10-30 10:17:26 +0300

    update project

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from './components/common/Logo.js';
+import Logo from './Logo.js';
 
 class Header extends React.Component {
 	render() {
--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './../../img/logo.png';
 
 class Logo extends React.Component {
 	render() {
 		return (
 			<div class="wrapper-img logo">
-				<img src="./img/logo.png" alt="logo"/>
+				<img src={logo} alt="logo"/>
 			</div>
 		)
 	}
