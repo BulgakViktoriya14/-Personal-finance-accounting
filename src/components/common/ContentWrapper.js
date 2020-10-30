01cd124d169263c4cb6659e5a49b1146bd985b34
BulgakViktoriya14 <bulgak.viktoriya@com.com>
2020-10-30 12:40:37 +0300

    update project

--- src/components/common/ContentWrapper.js
+++ src/components/common/ContentWrapper.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+
 import Sidebar from './Sidebar.js';
 
 class ContentWrapper extends React.Component {
@@ -6,6 +8,19 @@ class ContentWrapper extends React.Component {
 		return (
 			<section class="content">
 				<Sidebar/>
+				<Router>
+				<Switch>
+	        		<Route path="/about">
+	            		<h2>About</h2>
+	        		</Route>
+	        		<Route path="/users">
+	        			<h2>Users</h2>
+	        		</Route>
+	        		<Route path="/">
+	        			<h2>Home</h2>
+	        		</Route>
+	        	</Switch>
+	        	</Router>
 			</section>
 		)
 	}
--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+
 
 class Sidebar extends React.Component {
 	render() {
 		return (
-			<aside class="sidebar"></aside>
+			<aside class="sidebar">
+			 <Router>
+				<nav>
+		        	<ul>
+		        	<li>
+		        		<Link to="/">Home</Link>
+		            </li>
+		            <li>
+		        		<Link to="/about">About</Link>
+		        	</li>
+		            <li>
+		        		<Link to="/users">Users</Link>
+		            </li>
+		          </ul>
+		        </nav>
+		    </Router>
+			</aside>
 		)
 	}
 }
